19e552db6590951177a62d934aeccbc59edb85c3
Bryaan159 <00057720@uca.edu.sv>
2023-06-06 17:01:15 -0600

    Se agrego el codigo

--- clase/ejercicio.asm
+++ clase/ejercicio.asm
@@ -1,44 +1,39 @@
     org 100h
     section .text
-
-    XOR SI, SI
     XOR DI, DI
     MOV CX, 6d;
-    MOV AX, 0d;
+    MOV AX, 0;
     MOV BX, 01d;
 
 iterar:
-    ADD AX,BX
-    DEC CX
-    JNZ iterar 
+    ADC AX, [arreglo+DI]
+
+    INC DI
+    LOOP iterar
+
     ;verificar acarreo
-    ADC BX, 0
+    ; ADC BX, 0
     JNC no_carry
 
     SUB AX, BX
-    TEST AX,AX
-    JS no_positive
+    JS positive
     
-    ;Mueve el resultado a 210h
-    MOV [210h], AL
-    MOV AX, 1
-    XOR BX, BX
-    int 20h
 exit:
+
     MOV AX, 1
     XOR BX, BX
     int 20h
     
 section .data
+    arreglo db 1,34,3,4,50,66
 
-arreglo db 1,2,3,4,5,6
-
-    
 ;Las funciones
 
 no_carry:
     JMP exit
+    ret
 
-no_positive:
-    CMP AX,BX
-    JMP exit
+positive:
+    ;Mueve el resultado a 210h
+    MOV [210h], AX
+    ret
